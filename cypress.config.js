1f26933a36f0a5351e20e1dc254e34415b72a443
SparrowMike <michal.m_wroblewski@hotmail.com>
2022-12-07 22:42:31 +0800

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     // baseUrl: 'http://localhost:1337',
-    //baseUrl: 'http://tradingroom.sg',
+    //baseUrl: 'https://tradingroom.sg',
      baseUrl: 'https://genesiv.com',
-     "defaultCommandTimeout": 25000 
+//      "defaultCommandTimeout": 25000 
   }
 })
