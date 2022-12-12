f60b31b9cbefbd6af72fd65d6d34cb4a8d544c35
rahul rahul <rahul1988gupta88@gmail.com>
2022-12-12 11:10:14 +0530

    fixing the failing registration test

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   // These settings apply everywhere unless overridden
-  defaultCommandTimeout: 10000,
+  defaultCommandTimeout: 20000,
   viewportWidth: 1000,
   viewportHeight: 600,
   // Viewport settings overridden for component tests
@@ -12,8 +12,8 @@ module.exports = defineConfig({
   // Command timeout overridden for E2E tests
   e2e: {
     // baseUrl: 'http://localhost:1337',
-    baseUrl: 'https://tradingroom.sg',
+    //baseUrl: 'https://tradingroom.sg',
     // baseUrl: 'https://genesiv.com',    
-    defaultCommandTimeout: 10000
+    defaultCommandTimeout: 20000
   }
 })
